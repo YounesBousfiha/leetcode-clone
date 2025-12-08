b68984a2625036363eda44d1c5f06769d9aac675
Coderyounes <younesanas2021@gmail.com>
2025-12-08 23:33:18 +0100

    FEAT: add Spring Cloud Config Server with Vault and Git support

--- infrastructure/config-server/src/main/java/com/leetcode/configserver/ConfigServerApplication.java
+++ infrastructure/config-server/src/main/java/com/leetcode/configserver/ConfigServerApplication.java
@@ -4,11 +4,30 @@ package com.leetcode.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.environment.ConfigTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+    @Bean
+    public ConfigTokenProvider configTokenProvider() {
+        return new ConfigTokenProvider() {
+            @Override
+            public String getToken() {
+                String token = System.getenv("VAULT_TOKEN");
+
+                if (!StringUtils.hasText(token)) {
+                    throw new IllegalStateException("❌ Error: VAULT_TOKEN not found in environment variables!");
+                }
+
+                return token;
+            }
+        };
     }
 }
