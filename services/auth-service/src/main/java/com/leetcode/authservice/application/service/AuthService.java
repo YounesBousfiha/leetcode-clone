c5531d43f8c7ebeb27041a78c53b88efcf2cb3fa
Coderyounes <younesanas2021@gmail.com>
2025-12-09 23:53:26 +0100

    FEAT: add password reset functionality with token generation and email notification

--- services/auth-service/src/main/java/com/leetcode/authservice/application/service/AuthService.java
+++ services/auth-service/src/main/java/com/leetcode/authservice/application/service/AuthService.java
@@ -3,15 +3,20 @@ package com.leetcode.authservice.application.service;
 
 import com.leetcode.authservice.application.producer.EventPublisher;
 import com.leetcode.authservice.domain.entity.EmailVerification;
+import com.leetcode.authservice.domain.entity.PasswordResetToken;
 import com.leetcode.authservice.domain.entity.User;
 import com.leetcode.authservice.domain.enums.Role;
+import com.leetcode.authservice.domain.event.PasswordResetEvent;
 import com.leetcode.authservice.domain.event.UserRegisteredEvent;
 import com.leetcode.authservice.domain.exception.*;
 import com.leetcode.authservice.infrastructure.repository.EmailVerificationRepository;
+import com.leetcode.authservice.infrastructure.repository.PasswordResetTokenRepository;
 import com.leetcode.authservice.infrastructure.repository.UserRepository;
 import com.leetcode.authservice.infrastructure.security.JwtUtil;
+import com.leetcode.authservice.presentation.dto.request.ForgetPasswordRequest;
 import com.leetcode.authservice.presentation.dto.request.LoginRequest;
 import com.leetcode.authservice.presentation.dto.request.RegisterRequest;
+import com.leetcode.authservice.presentation.dto.request.ResetPasswordRequest;
 import com.leetcode.authservice.presentation.dto.response.LoginResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,6 +34,7 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final EmailVerificationRepository emailVerificationRepository;
+    private final PasswordResetTokenRepository resetTokenRepository;
     private final JwtUtil jwtUtil;
     private final EventPublisher eventPublisher;
 
@@ -119,5 +125,48 @@ public class AuthService {
 
 
         return "Account verified successfully. now You can Login";
+    }
+
+    public String initiatePasswordReset(ForgetPasswordRequest request) {
+        User user = this.userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        String token = UUID.randomUUID().toString();
+
+        PasswordResetToken resetToken = PasswordResetToken.builder()
+                .token(token)
+                .user(user)
+                .used(false)
+                .expiryDate(LocalDateTime.now().plusMinutes(30))
+                .build();
+
+        this.resetTokenRepository.save(resetToken);
+        PasswordResetEvent passwordResetEvent = PasswordResetEvent.builder()
+                .displayName(user.getDisplayName())
+                .email(user.getEmail())
+                .token(token)
+                .build();
+
+        eventPublisher.sendPasswordResetEvent(passwordResetEvent);
+
+        return "Password reset link sent to your email";
+    }
+
+    public String resetPassword(ResetPasswordRequest request ) {
+        PasswordResetToken resetToken = this.resetTokenRepository.findByToken(request.token())
+                .orElseThrow(() -> new RuntimeException("No such Token"));
+
+        if(resetToken.isUsed() || resetToken.getExpiryDate().isBefore(LocalDateTime.now())) {
+            throw new TokenExpire("Token Expired");
+        }
+
+        User user = resetToken.getUser();
+        user.setPassword(passwordEncoder.encode(request.newPassword()));
+        this.userRepository.save(user);
+
+        resetToken.setUsed(true);
+        this.resetTokenRepository.save(resetToken);
+
+        return "Password reset successfully. you can now Login";
     }
 }
