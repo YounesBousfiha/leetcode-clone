3d365ef94a373093c818ae02ec906d27e1e98c89
Coderyounes <younesanas2021@gmail.com>
2025-12-09 14:58:14 +0100

    FEAT: extend UserRepository to inherit from JpaRepository for improved data access

--- services/auth-service/src/main/java/com/leetcode/authservice/infrastructure/repository/UserRepository.java
+++ services/auth-service/src/main/java/com/leetcode/authservice/infrastructure/repository/UserRepository.java
@@ -2,12 +2,14 @@ package com.leetcode.authservice.infrastructure.repository;
 
 
 import com.leetcode.authservice.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
