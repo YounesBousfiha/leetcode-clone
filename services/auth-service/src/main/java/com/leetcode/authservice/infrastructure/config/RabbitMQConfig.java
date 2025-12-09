eea9b81bd2344172f01b63e3e87c934731514f15
Coderyounes <younesanas2021@gmail.com>
2025-12-09 15:30:03 +0100

    FEAT: add RabbitMQ configuration for message queuing

---
+++ services/auth-service/src/main/java/com/leetcode/authservice/infrastructure/config/RabbitMQConfig.java
@@ -0,0 +1,33 @@
+package com.leetcode.authservice.infrastructure.config;
+
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String EXCHANGE_NAME = "auth.exchange";
+    public static final String ROUTING_KEY = "auth.user.registered";
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageCOnverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageCOnverter());
+        return template;
+    }
+}
