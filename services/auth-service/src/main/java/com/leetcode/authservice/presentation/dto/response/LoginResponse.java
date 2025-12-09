c3207c747368de38ce9a46f9194e90748d4f0982
Coderyounes <younesanas2021@gmail.com>
2025-12-09 14:57:59 +0100

    FEAT: add Lombok @Builder annotation to LoginResponse for improved object creation

--- services/auth-service/src/main/java/com/leetcode/authservice/presentation/dto/response/LoginResponse.java
+++ services/auth-service/src/main/java/com/leetcode/authservice/presentation/dto/response/LoginResponse.java
@@ -1,5 +1,9 @@
 package com.leetcode.authservice.presentation.dto.response;
 
+
+import lombok.Builder;
+
+@Builder
 public record LoginResponse(
         String accessToken,
         String userId,
