154cd819aa9af3d17526b25918dd72ca29edc2f6
Coderyounes <younesanas2021@gmail.com>
2025-12-10 16:09:53 +0100

    FEAT: implement RedisService for managing refresh and access token storage

---
+++ services/auth-service/src/main/java/com/leetcode/authservice/infrastructure/service/RedisService.java
@@ -0,0 +1,46 @@
+package com.leetcode.authservice.infrastructure.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final StringRedisTemplate redisTemplate;
+    private static final String REFRESH_TOKEN = "REFRESH_TOKEN:";
+
+    public void saveRefreshToken(String email, String refreshToken) {
+        redisTemplate.opsForValue().set(
+                REFRESH_TOKEN + refreshToken,
+                email,
+                Duration.ofDays(7)
+        );
+    }
+
+    public String extractRefreshTokenSubject(String refreshToken) {
+        return redisTemplate.opsForValue().get(REFRESH_TOKEN + refreshToken);
+    }
+
+    public void deleteRefreshToken(String refreshToken) {
+        redisTemplate.delete(REFRESH_TOKEN + refreshToken);
+    }
+
+    public void blackListAccessToken(String accessToken, long ttlInMillis) {
+        if(ttlInMillis > 0) {
+            redisTemplate.opsForValue().set(
+                    "BLACKLIST:" + accessToken,
+                    "true",
+                    Duration.ofMillis(ttlInMillis)
+            );
+        }
+    }
+
+    public boolean isTokenBlackListed(String accessToken) {
+        return  Boolean.TRUE.equals(redisTemplate.hasKey("BLACKLIST:" + accessToken));
+    }
+}
