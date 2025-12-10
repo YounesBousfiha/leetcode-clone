c2bdac1f504eebb504db0139124c020bb6100159
Coderyounes <younesanas2021@gmail.com>
2025-12-10 20:29:47 +0100

    FEAT: add RouteValidator for securing API endpoints

---
+++ infrastructure/api-gateway/src/main/java/com/leetcode/gateway/infrastructure/filter/RouteValidator.java
@@ -0,0 +1,26 @@
+package com.leetcode.gateway.infrastructure.filter;
+
+import
+        org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/api/auth/register",
+            "/api/auth/login",
+            "/api/auth/verify",
+            "/api/auth/refresh-token",
+            "/api/auth/forgot-password",
+            "/api/auth/reset-password",
+            "/eureka"
+
+    );
+
+    public Predicate<ServerHttpRequest> isSecured = request -> openApiEndpoints.stream()
+            .noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
