1d63ac1c6a4a16c031876875b9c5c621e4414b11
Coderyounes <younesanas2021@gmail.com>
2025-12-11 00:26:45 +0100

    FEAT: configure stateless session management in SecurityConfig

--- services/auth-service/src/main/java/com/leetcode/authservice/infrastructure/config/SecurityConfig.java
+++ services/auth-service/src/main/java/com/leetcode/authservice/infrastructure/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +24,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 
