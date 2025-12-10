499ebb06fe5e2be7b35a0b41293923b8ba7a7287
Coderyounes <younesanas2021@gmail.com>
2025-12-10 20:29:06 +0100

    FEAT: add refreshToken field to LoginResponse and create LogoutRequest record

--- services/auth-service/src/main/java/com/leetcode/authservice/presentation/dto/response/LoginResponse.java
+++ services/auth-service/src/main/java/com/leetcode/authservice/presentation/dto/response/LoginResponse.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 @Builder
 public record LoginResponse(
         String accessToken,
+        String refreshToken,
         String userId,
         String email,
         String role
