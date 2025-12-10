087800427ee30099903dc88b8a5cf8f2b5f8a865
Coderyounes <younesanas2021@gmail.com>
2025-12-10 20:29:53 +0100

    FEAT: add AutheticationFilter for JWT validation and user role extraction

---
+++ infrastructure/api-gateway/src/main/java/com/leetcode/gateway/infrastructure/filter/AutheticationFilter.java
@@ -0,0 +1,82 @@
+package com.leetcode.gateway.infrastructure.filter;
+
+import com.leetcode.gateway.infrastructure.security.JwtUtil;
+import com.leetcode.gateway.infrastructure.security.RedisService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Slf4j
+@Component
+public class AutheticationFilter extends AbstractGatewayFilterFactory<AutheticationFilter.Config> {
+
+
+    @Autowired
+    private RouteValidator validator;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private RedisService redisService;
+
+    public AutheticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if(validator.isSecured.test(exchange.getRequest())) {
+                if(exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    return onError(exchange, "Missing Authorization header", HttpStatus.UNAUTHORIZED);
+                }
+
+                String authHeader = Objects.requireNonNull(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION)).getFirst();
+
+                if(authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+
+                try {
+                    jwtUtil.validateToken(authHeader);
+
+                    if(redisService.isTokenBlackListed(authHeader)) {
+                        throw new RuntimeException("Token is BlackListed (Logged out)");
+                    }
+
+                    String userId = jwtUtil.extractUserId(authHeader);
+                    String role = jwtUtil.extractRole(authHeader);
+
+                    ServerHttpRequest request = exchange.getRequest()
+                            .mutate()
+                            .header("X-User-Id", userId)
+                            .header("X-User-Role", role)
+                            .build();
+                    return  chain.filter(exchange.mutate().request(request).build());
+                } catch (Exception e) {
+                    log.error("Access Denied: {}", e.getMessage() );
+                    return onError(exchange, "Unauthorized access: " + e.getMessage(), HttpStatus.UNAUTHORIZED);
+                }
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        exchange.getResponse().setStatusCode(httpStatus);
+        return exchange.getResponse().setComplete();
+    }
+
+    public static class Config {}
+}
