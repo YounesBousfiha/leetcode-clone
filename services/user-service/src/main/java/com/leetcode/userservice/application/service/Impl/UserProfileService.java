4d2e826ae8c61865cb411b3e700066b07262fd24
Coderyounes <younesanas2021@gmail.com>
2026-02-07 12:18:38 +0100

    FEAT: implement updateProfile and getProfileById methods in UserProfileService

--- services/user-service/src/main/java/com/leetcode/userservice/application/service/Impl/UserProfileService.java
+++ services/user-service/src/main/java/com/leetcode/userservice/application/service/Impl/UserProfileService.java
@@ -1,13 +1,17 @@
 package com.leetcode.userservice.application.service.Impl;
 
 
+import com.leetcode.userservice.application.mapper.UserProfileMapper;
 import com.leetcode.userservice.application.service.IUserProfileService;
 import com.leetcode.userservice.domain.entity.UserProfile;
 import com.leetcode.userservice.domain.event.UserRegisteredEvent;
 import com.leetcode.userservice.infrastructure.repository.UserProfileRepository;
+import com.leetcode.userservice.prensetation.dto.UpdateProfileRequest;
+import com.leetcode.userservice.prensetation.dto.UserProfileResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -17,8 +21,10 @@ import java.util.UUID;
 public class UserProfileService implements IUserProfileService {
 
     private final UserProfileRepository userProfileRepository;
+    private final UserProfileMapper mapper;
 
     @Override
+    @Transactional
     public void createNewProfile(UserRegisteredEvent event) {
 
         log.info("Creating profile for user: {}", event.userId());
@@ -37,5 +43,27 @@ public class UserProfileService implements IUserProfileService {
 
         this.userProfileRepository.save(newProfile);
         log.info("Profile created successfully for user : {}", event.email());
+    }
+
+    @Override
+    @Transactional
+    public UserProfileResponse updateProfile(String id, UpdateProfileRequest request) {
+        UserProfile profile = userProfileRepository.findById(UUID.fromString(id))
+                .orElseThrow(() -> new RuntimeException("Profile not found"));
+
+        mapper.updateEntityFromRequest(request, profile);
+
+        UserProfile savedProfile = userProfileRepository.save(profile);
+
+        return mapper.toResponse(savedProfile);
+
+    }
+
+    @Override
+    public UserProfileResponse getProfileById(String id) {
+        UserProfile profile = userProfileRepository.findById(UUID.fromString(id))
+                .orElseThrow(() -> new RuntimeException("Profile not found"));
+
+        return mapper.toResponse(profile);
     }
 }
