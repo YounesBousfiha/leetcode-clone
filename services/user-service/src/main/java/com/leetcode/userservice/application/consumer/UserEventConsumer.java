2aa43768cdd1e322321dd17d46efe583a8c73ac1
Coderyounes <younesanas2021@gmail.com>
2026-02-07 11:47:48 +0100

    FEAT: integrate IUserProfileService for user profile creation in UserEventConsumer

--- services/user-service/src/main/java/com/leetcode/userservice/application/consumer/UserEventConsumer.java
+++ services/user-service/src/main/java/com/leetcode/userservice/application/consumer/UserEventConsumer.java
@@ -1,9 +1,12 @@
 package com.leetcode.userservice.application.consumer;
 
+import com.leetcode.userservice.application.service.IUserProfileService;
 import com.leetcode.userservice.domain.entity.UserProfile;
 import com.leetcode.userservice.domain.event.UserRegisteredEvent;
 import com.leetcode.userservice.infrastructure.repository.UserProfileRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -11,19 +14,20 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserEventConsumer {
 
-    private final UserProfileRepository userProfileRepository;
+    private final IUserProfileService userProfileService;
 
     @RabbitListener(queues = "user.profile.queue")
     public void handleUserRegistered(UserRegisteredEvent event) {
+        try {
+            log.info("Received UserRegisteredEvent for email: {}", event.email());
+            userProfileService.createNewProfile(event);
+        } catch (Exception e) {
+            log.error("Error creating user profile: {}", e.getMessage());
 
-        UserProfile profile = UserProfile.builder()
-                .id(UUID.fromString(event.userId()))
-                .email(event.email())
-                .displayName(event.displayName())
-                .score(0L)
-                .build();
-        userProfileRepository.save(profile);
+            throw new AmqpRejectAndDontRequeueException("Moving to DLQ", e);
+        }
     }
 }
