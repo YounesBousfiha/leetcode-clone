6bb432f16ce3e6df2ec82cba150f2896300f03dc
Coderyounes <younesanas2021@gmail.com>
2026-02-07 12:17:04 +0100

    FEAT: add UserProfileMapper for mapping UserProfile and UpdateProfileRequest

---
+++ services/user-service/src/main/java/com/leetcode/userservice/application/mapper/UserProfileMapper.java
@@ -0,0 +1,29 @@
+package com.leetcode.userservice.application.mapper;
+
+
+import com.leetcode.userservice.domain.entity.UserProfile;
+import com.leetcode.userservice.domain.event.UserRegisteredEvent;
+import com.leetcode.userservice.prensetation.dto.UpdateProfileRequest;
+import com.leetcode.userservice.prensetation.dto.UserProfileResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface UserProfileMapper {
+
+
+    @Mapping(target = "id", expression = "java(java.util.UUID.fromString(event.userId()))")
+    @Mapping(target = "score", constant = "0L")
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
+    @Mapping(target = "bio", ignore = true)
+    @Mapping(target = "githubUrl", ignore = true)
+    @Mapping(target = "linkedinUrl", ignore = true)
+    UserProfile toEntity(UserRegisteredEvent event);
+
+    UserProfileResponse toResponse(UserProfile userProfile);
+
+
+    void updateEntityFromRequest(UpdateProfileRequest request, @MappingTarget UserProfile entity);
+}
