8c4828d677d2227fb10d80d03681848f7a87531b
Coderyounes <younesanas2021@gmail.com>
2026-02-07 12:07:22 +0100

    FEAT: add updateProfile and getProfileById methods to IUserProfileService

--- services/user-service/src/main/java/com/leetcode/userservice/application/service/IUserProfileService.java
+++ services/user-service/src/main/java/com/leetcode/userservice/application/service/IUserProfileService.java
@@ -2,8 +2,12 @@ package com.leetcode.userservice.application.service;
 
 import com.leetcode.userservice.domain.entity.UserProfile;
 import com.leetcode.userservice.domain.event.UserRegisteredEvent;
+import com.leetcode.userservice.prensetation.dto.UpdateProfileRequest;
+import com.leetcode.userservice.prensetation.dto.UserProfileResponse;
 
 public interface IUserProfileService {
 
     void createNewProfile(UserRegisteredEvent data);
+    UserProfileResponse updateProfile(String id, UpdateProfileRequest request);
+    public UserProfileResponse getProfileById(String id);
 }
