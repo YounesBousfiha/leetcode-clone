b5f2a65a46b7b2afc36478b19b137efab6d037fb
Coderyounes <younesanas2021@gmail.com>
2026-02-08 13:26:35 +0100

    FEAT: add Problem entity with attributes and relationships for problem management

---
+++ services/problem-service/src/main/java/com/leetcode/problemservice/domain/entity/Problem.java
@@ -0,0 +1,70 @@
+package com.leetcode.problemservice.domain.entity;
+
+
+import com.leetcode.problemservice.domain.enums.Difficulty;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.*;
+
+@Entity
+@Table(name = "problems")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Problem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
+
+    private Double timeLimit;
+
+    private Integer memoryLimit;
+
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name= "problem_tags",
+            joinColumns = @JoinColumn(name= "problem_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    @Builder.Default
+    private Set<Tag> tags = new HashSet<>();
+
+    @OneToMany(mappedBy = "problem", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<TestCase> testCases = new ArrayList<>();
+
+
+    @ElementCollection
+    @CollectionTable(name = "problem_hints", joinColumns = @JoinColumn(name = "problem_id"))
+    @Column(name = "hints")
+    @Builder.Default
+    private List<String> hints = new ArrayList<>();
+
+
+    public void addTestCase(TestCase testCase) {
+        testCases.add(testCase);
+        testCase.setProblem(this);
+    }
+
+    public void removeTestCase(TestCase testCase) {
+        testCases.remove(testCase);
+        testCase.setProblem(null);
+    }
+}
