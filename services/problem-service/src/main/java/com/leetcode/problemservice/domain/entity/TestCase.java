852bc0710aa229fbfa16c2a8ee5782678513c7b9
Coderyounes <younesanas2021@gmail.com>
2026-02-08 13:26:55 +0100

    FEAT: add TestCase entity for managing test cases associated with problems

---
+++ services/problem-service/src/main/java/com/leetcode/problemservice/domain/entity/TestCase.java
@@ -0,0 +1,34 @@
+package com.leetcode.problemservice.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "test_cases")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TestCase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String input;
+
+    @Column(nullable = false)
+    private String expectedOutput;
+
+    private boolean isPublic;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "problem_id")
+    @ToString.Exclude
+    private Problem problem;
+}
