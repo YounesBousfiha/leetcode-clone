3b44df13e41eb756bf500f17f63db62980b28f1b
Coderyounes <younesanas2021@gmail.com>
2026-02-08 10:44:23 +0100

    FEAT: change return type of findByUserProfileIdAndProblemId method to Optional in UserNoteRepository

--- services/user-service/src/main/java/com/leetcode/userservice/infrastructure/repository/UserNoteRepository.java
+++ services/user-service/src/main/java/com/leetcode/userservice/infrastructure/repository/UserNoteRepository.java
@@ -4,9 +4,10 @@ import com.leetcode.userservice.domain.entity.UserNote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserNoteRepository extends JpaRepository<UserNote, String> {
-    UserNote findByUserProfileIdAndProblemId(UUID userId, String problemId);
+    Optional<UserNote> findByUserProfileIdAndProblemId(UUID userId, String problemId);
 }
