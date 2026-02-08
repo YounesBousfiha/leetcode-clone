7c1e0a41767c94d28e275f109df1af179ae470ad
Coderyounes <younesanas2021@gmail.com>
2026-02-08 10:38:05 +0100

    FEAT: add updateEntity method to UserNoteMapper for updating UserNote entities

--- services/user-service/src/main/java/com/leetcode/userservice/application/mapper/UserNoteMapper.java
+++ services/user-service/src/main/java/com/leetcode/userservice/application/mapper/UserNoteMapper.java
@@ -2,9 +2,10 @@ package com.leetcode.userservice.application.mapper;
 
 
 import com.leetcode.userservice.domain.entity.UserNote;
+import com.leetcode.userservice.prensetation.dto.UserNoteRequest;
 import com.leetcode.userservice.prensetation.dto.UserNoteResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.NullValueMappingStrategy;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
@@ -14,4 +15,12 @@ public interface UserNoteMapper {
 
 
     UserNoteResponse toResponse(UserNote note);
+
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "userProfile", ignore = true)
+    @Mapping(target = "lastUpdated", ignore = true)
+    void updateEntity(UserNoteRequest request, UserNote userNote);
 }
+
+
