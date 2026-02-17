34d723e37acdfa605f9c8c26b90c0d842392927d
Coderyounes <younesanas2021@gmail.com>
2026-02-17 15:38:19 +0000

    FEAT: add IProblemService interface for problem management operations

---
+++ services/problem-service/src/main/java/com/leetcode/problemservice/application/service/IProblemService.java
@@ -0,0 +1,18 @@
+package com.leetcode.problemservice.application.service;
+
+import com.leetcode.problemservice.prensentation.dto.CreateProblemRequest;
+import com.leetcode.problemservice.prensentation.dto.ProblemDetailResponse;
+import com.leetcode.problemservice.prensentation.dto.ProblemListResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IProblemService {
+    ProblemDetailResponse createProblem(CreateProblemRequest request);
+
+    Page<ProblemListResponse> getAllProblems(Pageable pageable);
+
+    ProblemDetailResponse getProblemBySlug(String slug);
+
+    void deleteProblem(String id);
+
+}
