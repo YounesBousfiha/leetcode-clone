2f1e31b153683f82f878b755d1ebc713167eaaeb
Coderyounes <younesanas2021@gmail.com>
2026-02-17 15:51:41 +0000

    FEAT: add ProblemController for problem management endpoints

---
+++ services/problem-service/src/main/java/com/leetcode/problemservice/prensentation/controller/ProblemController.java
@@ -0,0 +1,51 @@
+package com.leetcode.problemservice.prensentation.controller;
+
+
+import com.leetcode.problemservice.application.service.IProblemService;
+import com.leetcode.problemservice.prensentation.dto.CreateProblemRequest;
+import com.leetcode.problemservice.prensentation.dto.ProblemDetailResponse;
+import com.leetcode.problemservice.prensentation.dto.ProblemListResponse;
+import jakarta.ws.rs.Path;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/problems")
+@RequiredArgsConstructor
+public class ProblemController {
+
+    private final IProblemService problemService;
+
+    @PostMapping
+    public ResponseEntity<ProblemDetailResponse> createProblem(@RequestBody CreateProblemRequest request) {
+        ProblemDetailResponse response = problemService.createProblem(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProblemListResponse>> getAllProblems(
+            @PageableDefault(size = 20)Pageable pageable
+            ) {
+        return ResponseEntity.ok(problemService.getAllProblems(pageable));
+    }
+
+    @GetMapping("{slug}")
+    public ResponseEntity<ProblemDetailResponse> getProblemBySlug(
+            @PathVariable String slug
+    ) {
+        return ResponseEntity.ok(problemService.getProblemBySlug(slug));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProblem(@PathVariable String id) {
+        problemService.deleteProblem(id);
+        return ResponseEntity.noContent().build();
+    }
+}
