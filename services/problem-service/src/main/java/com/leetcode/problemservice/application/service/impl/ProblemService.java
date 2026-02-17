26b7f0c61455c191f21c7fa91e32544ff24c6420
Coderyounes <younesanas2021@gmail.com>
2026-02-17 15:38:28 +0000

    FEAT: implement ProblemService for problem creation and management

---
+++ services/problem-service/src/main/java/com/leetcode/problemservice/application/service/impl/ProblemService.java
@@ -0,0 +1,99 @@
+package com.leetcode.problemservice.application.service.impl;
+
+import com.leetcode.problemservice.application.mapper.ProblemMapper;
+import com.leetcode.problemservice.application.service.IProblemService;
+import com.leetcode.problemservice.domain.entity.CodeTemplate;
+import com.leetcode.problemservice.domain.entity.Problem;
+import com.leetcode.problemservice.domain.entity.Tag;
+import com.leetcode.problemservice.domain.entity.TestCase;
+import com.leetcode.problemservice.infrastcture.repository.ProblemRepository;
+import com.leetcode.problemservice.infrastcture.repository.TagRepository;
+import com.leetcode.problemservice.prensentation.dto.CreateProblemRequest;
+import com.leetcode.problemservice.prensentation.dto.ProblemDetailResponse;
+import com.leetcode.problemservice.prensentation.dto.ProblemListResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProblemService implements IProblemService {
+
+    private final ProblemRepository problemRepository;
+    private  final TagRepository tagRepository;
+    private ProblemMapper problemMapper;
+
+    @Override
+    public ProblemDetailResponse createProblem(CreateProblemRequest request) {
+        log.info("Create new Problem: {}", request.title());
+
+        String slug = request.title().toLowerCase().replace(" ", "-");
+
+        if(problemRepository.existsBySlug(slug)) {
+            throw new RuntimeException("Problem with this title already exists");
+        }
+
+        Problem problem = problemMapper.toEntity(request);
+        problem.setSlug(slug);
+
+        Set<Tag> tags = request.tags().stream()
+                .map(tagName -> tagRepository.findByName(tagName)
+                        .orElseGet(() -> {
+                            Tag newTag = Tag.builder()
+                                    .name(tagName)
+                                    .slug(tagName.toLowerCase().replace(" ", "-"))
+                                    .build();
+                            return tagRepository.save(newTag);
+                        })
+                )
+                .collect(Collectors.toSet());
+        problem.setTags(tags);
+
+        if(request.testCase() != null) {
+            request.testCase().forEach(tcDto -> {
+                TestCase testCase = TestCase.builder()
+                        .input(tcDto.input())
+                        .expectedOutput(tcDto.expectedOutput())
+                        .isPublic(tcDto.isPublic())
+                        .build();
+                problem.addTestCase(testCase);
+            });
+        }
+
+        if(request.codeTemplate() != null) {
+            request.codeTemplate().forEach(ctDto -> {
+                CodeTemplate template = CodeTemplate.builder()
+                        .language(ctDto.language())
+                        .codeBoilerplate(ctDto.codeBoilerplate())
+                        .build();
+                problem.addTemplate(template);
+            });
+        }
+
+        Problem savedProblem = problemRepository.save(problem);
+        log.info("Problem created with ID: {}", savedProblem.getId());
+
+        return problemMapper.toDetailResponse(savedProblem);
+    }
+
+    @Override
+    public Page<ProblemListResponse> getAllProblems(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public ProblemDetailResponse getProblemBySlug(String slug) {
+        return null;
+    }
+
+    @Override
+    public void deleteProblem(String id) {
+
+    }
+}
