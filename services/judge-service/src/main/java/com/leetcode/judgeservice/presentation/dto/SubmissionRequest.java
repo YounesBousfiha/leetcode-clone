8b9acd851d8be7c63cc4da7260733f0f00390ded
Coderyounes <younesanas2021@gmail.com>
2026-02-18 14:39:23 +0000

    FEAT: add SubmissionRequest DTO for submission data validation

---
+++ services/judge-service/src/main/java/com/leetcode/judgeservice/presentation/dto/SubmissionRequest.java
@@ -0,0 +1,16 @@
+package com.leetcode.judgeservice.presentation.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+
+@Builder
+public record SubmissionRequest(
+        @NotBlank(message = "Code cannot be empty")
+        String code,
+
+        @NotBlank(message = "Language is required")
+        String language,
+
+        @NotBlank(message = "problem ID is required")
+        String problemId) {
+}
