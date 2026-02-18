244f1614942f5a0ee4a15a031c96043a6150839e
Coderyounes <younesanas2021@gmail.com>
2026-02-18 15:41:34 +0000

    FEAT: enable OpenFeign clients in JudgeApplication

--- services/judge-service/src/main/java/com/leetcode/judgeservice/JudgeApplication.java
+++ services/judge-service/src/main/java/com/leetcode/judgeservice/JudgeApplication.java
@@ -2,8 +2,10 @@ package com.leetcode.judgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class JudgeApplication {
     public static void main(String[] args) {
         SpringApplication.run(JudgeApplication.class, args);
