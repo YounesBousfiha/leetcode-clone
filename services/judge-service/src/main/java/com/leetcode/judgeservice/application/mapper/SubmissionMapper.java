a332a5fbdc2c3a9d5e9cd45de79de95dd922a65e
Coderyounes <younesanas2021@gmail.com>
2026-02-18 14:39:39 +0000

    FEAT: add SubmissionMapper for mapping between DTOs and entities

---
+++ services/judge-service/src/main/java/com/leetcode/judgeservice/application/mapper/SubmissionMapper.java
@@ -0,0 +1,30 @@
+package com.leetcode.judgeservice.application.mapper;
+
+
+import com.leetcode.judgeservice.domain.entity.Submission;
+import com.leetcode.judgeservice.domain.entity.SubmissionResult;
+import com.leetcode.judgeservice.presentation.dto.SubmissionRequest;
+import com.leetcode.judgeservice.presentation.dto.SubmissionResponse;
+import com.leetcode.judgeservice.presentation.dto.SubmissionResultResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface SubmissionMapper {
+
+
+    @Mapping(target = "details", source = "resultList")
+    SubmissionResponse toResponse(Submission submission);
+
+
+    SubmissionResultResponse toResultResponse(SubmissionResult result);
+
+
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", constant = "PENDING")
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "userId" ,ignore = true)
+    @Mapping(target = "language", expression = "java(com.leetcode.judgeservice.domain.enums.ProgrammingLanguage)")
+    Submission toEntity(SubmissionRequest request);
+}
