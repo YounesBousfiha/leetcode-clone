6b9612e09af0a92003163d85cfff75e2dd06a1b4
Coderyounes <younesanas2021@gmail.com>
2026-02-18 18:49:58 +0000

    FEAT: implement PythonStrategy for Python language execution

---
+++ services/judge-service/src/main/java/com/leetcode/judgeservice/infrastructure/execution/strategy/PythonStrategy.java
@@ -0,0 +1,34 @@
+package com.leetcode.judgeservice.infrastructure.execution.strategy;
+
+import com.leetcode.judgeservice.domain.enums.ProgrammingLanguage;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PythonStrategy  implements ILanguagesStrategy{
+    @Override
+    public ProgrammingLanguage getLanguage() {
+        return ProgrammingLanguage.PYTHON;
+    }
+
+    @Override
+    public String getDockerImage() {
+        return "python:3.9-alpine";
+    }
+
+    @Override
+    public String getFileExtension() {
+        return ".py";
+    }
+
+    @Override
+    public String getRunCommand() {
+        return "python3 main.py";
+    }
+
+    @Override
+    public String wrapCode(String userCode) {
+        return "import sys\n" +
+                "import math\n" +
+                userCode;
+    }
+}
