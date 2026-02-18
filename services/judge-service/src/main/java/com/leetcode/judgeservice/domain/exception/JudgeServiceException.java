3940561939cef293126bc3b55f150f79a8878907
Coderyounes <younesanas2021@gmail.com>
2026-02-18 14:29:25 +0000

    FEAT: add JudgeServiceException for handling service-specific errors

---
+++ services/judge-service/src/main/java/com/leetcode/judgeservice/domain/exception/JudgeServiceException.java
@@ -0,0 +1,7 @@
+package com.leetcode.judgeservice.domain.exception;
+
+public class JudgeServiceException extends RuntimeException {
+    public JudgeServiceException(String message) {
+        super(message);
+    }
+}
