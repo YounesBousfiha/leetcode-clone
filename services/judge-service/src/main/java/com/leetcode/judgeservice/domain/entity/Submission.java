5faf15c72e7838af75a47b355c0c10b0a3059b28
Coderyounes <younesanas2021@gmail.com>
2026-02-18 14:17:20 +0000

    FEAT: create Submission entity for managing code submissions

---
+++ services/judge-service/src/main/java/com/leetcode/judgeservice/domain/entity/Submission.java
@@ -0,0 +1,59 @@
+package com.leetcode.judgeservice.domain.entity;
+
+
+import com.leetcode.judgeservice.domain.enums.ProgrammingLanguage;
+import com.leetcode.judgeservice.domain.enums.SubmissionStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "submissions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Submission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID userID;
+
+    @Column(nullable = false)
+    private String problemId;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String code;
+
+    @Enumerated(EnumType.STRING)
+    private ProgrammingLanguage language;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private SubmissionStatus status = SubmissionStatus.PENDING;
+
+    private Double executionTime;
+    private long memoryUsed;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private LocalDateTime completedAt;
+
+    @OneToMany(mappedBy = "submission", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SubmissionResult> resultList = new ArrayList<>();
+
+    public void addResult(SubmissionResult result) {
+        resultList.add(result);
+        result.setSubmission(this);
+    }
+}
