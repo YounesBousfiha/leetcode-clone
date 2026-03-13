87489e4e4c0c347e895c122cc7e2577d16cfd522
Younes Bousfiha <125454968+YounesBousfiha@users.noreply.github.com>
2026-03-13 09:08:47 +0000

    FEAT: add difficulty field to InternalProblemResponse record

--- services/problem-service/src/main/java/com/leetcode/problemservice/prensentation/dto/InternalProblemResponse.java
+++ services/problem-service/src/main/java/com/leetcode/problemservice/prensentation/dto/InternalProblemResponse.java
@@ -9,6 +9,7 @@ import java.util.List;
 public record InternalProblemResponse(
         String id,
         String slug,
+        String difficulty,
         Double timeLimit,
         Integer memoryLimit,
         List<TestCaseDto> testCases
